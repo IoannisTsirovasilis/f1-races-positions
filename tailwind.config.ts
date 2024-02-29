58866590144aeaa06431f92780bc329593383971
Ioannis Tsirovasilis <ioannis.tsirovasilis@di-coders.com>
2024-03-01 01:39:54 +0200

    Remove background image from tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'bg-image': "url('/background.jpg')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
